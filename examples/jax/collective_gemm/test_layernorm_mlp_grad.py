9de9cc8afb616e53cd09bc0277d367654d552b1b
Phuong Nguyen <phuonguyen@nvidia.com>
2025-09-05 12:49:26 -0700

    fix test
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- examples/jax/collective_gemm/test_layernorm_mlp_grad.py
+++ examples/jax/collective_gemm/test_layernorm_mlp_grad.py
@@ -48,22 +48,22 @@ assert (
 
 
 def _get_logical_axes():
-        input_axes_1 = (NAME_DP_AXIS, NAME_TPSP_AXIS, None)
-        weight_axes_1 = (None, NAME_TPSP_AXIS)
-        bias_axes_1 = (NAME_TPSP_AXIS,)
-        input_axes_2 = (NAME_DP_AXIS, None, None)
-        weight_axes_2 = (NAME_TPSP_AXIS, None)
-        bias_axes_2 = (None,)
-        output_axes = (NAME_DP_AXIS, NAME_TPSP_AXIS, None)
-    return input_axes_1, weight_axes_1, bias_axes_1, input_axes_2, weight_axes_2, bias_axes_2, output_axes
+    input_1_axes = (NAME_DP_AXIS, NAME_TPSP_AXIS, None)
+    weight_1_axes = (None, None, NAME_TPSP_AXIS)
+    bias_axes_1 = (None, NAME_TPSP_AXIS)
+    input_2_axes = (NAME_DP_AXIS, None, None)
+    weight_2_axes = (NAME_TPSP_AXIS, None)
+    bias_axes_2 = (None,)
+    output_axes = (NAME_DP_AXIS, NAME_TPSP_AXIS, None)
+    return input_1_axes, weight_1_axes, bias_axes_1, input_2_axes, weight_2_axes, bias_axes_2, output_axes
 
 
 def _get_operand_sharding(mesh):
-    input_axes_1, weight_axes_1, bias_axes_1, input_axes_2, weight_axes_2, bias_axes_2, _ = _get_logical_axes()
-    x_sharding = NamedSharding(mesh, PartitionSpec(*input_axes))
-    weight_1_sharding = NamedSharding(mesh, PartitionSpec(*weight_axes_1))
+    input_1_axes, weight_1_axes, bias_axes_1, input_2_axes, weight_2_axes, bias_axes_2, _ = _get_logical_axes()
+    x_sharding = NamedSharding(mesh, PartitionSpec(*input_1_axes))
+    weight_1_sharding = NamedSharding(mesh, PartitionSpec(*weight_1_axes))
     bias_1_sharding = NamedSharding(mesh, PartitionSpec(*bias_axes_1))
-    weight_2_sharding = NamedSharding(mesh, PartitionSpec(*weight_axes_2))
+    weight_2_sharding = NamedSharding(mesh, PartitionSpec(*weight_2_axes))
     bias_2_sharding = NamedSharding(mesh, PartitionSpec(*bias_axes_2))
     return x_sharding, weight_1_sharding, bias_1_sharding, weight_2_sharding, bias_2_sharding
 
@@ -91,15 +91,15 @@ def _create_mesh(args):
 def _mean_layernorm_mlp(x, weight_1, bias_1, weight_2, bias_2, input_1_axes, input_2_axes, weight_1_axes, weight_2_axes, output_axes, cgemm_config_sets):
     output = layernorm_mlp(
         x,
-        weight_1,
-        bias_1,
-        weight_2,
-        bias_2,
+        kernels=[weight_1, weight_2],
+        biases=[bias_1, bias_2],
+        norm_type="rmsnorm",
         dot_1_input_axes=input_1_axes,
         dot_2_input_axes=input_2_axes,
         kernel_1_axes=weight_1_axes,
         kernel_2_axes=weight_2_axes,
         output_axes=output_axes,
+        activation_type=("gelu"),
         cgemm_config_sets=cgemm_config_sets,
     )
     return jnp.mean(output)
@@ -124,8 +124,8 @@ def run_layernorm_mlp_grad_tests(args, mesh=None):
     x = jax.random.normal(
         x_rng, (args.batch_size, args.seq_len, args.hidden_in), dtype=jnp.bfloat16
     )
-    weight_1 = jax.random.normal(weight_1_rng, (args.hidden_in, args.hidden_out), dtype=jnp.bfloat16)
-    bias_1 = jax.random.normal(bias_1_rng, (args.hidden_out,), dtype=jnp.bfloat16)
+    weight_1 = jax.random.normal(weight_1_rng, (args.hidden_in, 1, args.hidden_out), dtype=jnp.bfloat16)
+    bias_1 = jax.random.normal(bias_1_rng, (1, args.hidden_out), dtype=jnp.bfloat16)
     weight_2 = jax.random.normal(weight_2_rng, (args.hidden_out, args.hidden_in), dtype=jnp.bfloat16)
     bias_2 = jax.random.normal(bias_2_rng, (args.hidden_in,), dtype=jnp.bfloat16)
 
@@ -143,6 +143,7 @@ def run_layernorm_mlp_grad_tests(args, mesh=None):
             cgemm_config_set_1 = CollectiveGemmConfigSet.create(forward_collective_op=CollectiveOp.ALL_GATHER)
             cgemm_config_set_2 = CollectiveGemmConfigSet.create(forward_collective_op=CollectiveOp.REDUCE_SCATTER)
             cgemm_config_sets = (cgemm_config_set_1, cgemm_config_set_2)
+            noop_cgemm_config_sets = (noop_cgemm_config_set, noop_cgemm_config_set)
 
             x_sharding, weight_1_sharding, bias_1_sharding, weight_2_sharding, bias_2_sharding = _get_operand_sharding(mesh)
             x_sharded = jax.device_put(x, x_sharding)
@@ -151,12 +152,12 @@ def run_layernorm_mlp_grad_tests(args, mesh=None):
             weight_2_sharded = jax.device_put(weight_2, weight_2_sharding)
             bias_2_sharded = jax.device_put(bias_2, bias_2_sharding)
 
-            input_axes_1, weight_axes_1, _, input_axes_2, weight_axes_2, _, output_axes = _get_logical_axes()
+            input_1_axes, weight_1_axes, _, input_2_axes, weight_2_axes, _, output_axes = _get_logical_axes()
             ref_output, ref_grads = _value_and_grad_layernorm_mlp(
-                x_sharded, weight_1_sharded, bias_1_sharded, weight_2_sharded, bias_2_sharded, input_axes_1, input_axes_2, weight_1_axes, weight_2_axes, output_axes, noop_cgemm_config_sets,
+                x_sharded, weight_1_sharded, bias_1_sharded, weight_2_sharded, bias_2_sharded, input_1_axes, input_2_axes, weight_1_axes, weight_2_axes, output_axes, noop_cgemm_config_sets,
             )
             output, sharded_grads = _value_and_grad_layernorm_mlp(
-                x_sharded, weight_1_sharded, bias_1_sharded, weight_2_sharded, bias_2_sharded, input_axes_1, input_axes_2, weight_1_axes, weight_2_axes, output_axes, cgemm_config_sets
+                x_sharded, weight_1_sharded, bias_1_sharded, weight_2_sharded, bias_2_sharded, input_1_axes, input_2_axes, weight_1_axes, weight_2_axes, output_axes, cgemm_config_sets
             )
         jax.block_until_ready(ref_output)
         jax.block_until_ready(output)
@@ -251,29 +252,29 @@ class TestCollectiveDenseGradient(unittest.TestCase):
         run_layernorm_mlp_grad_tests(self.args, self.mesh)
 
 
-class TestCollectiveDenseGradientWithDP(unittest.TestCase):
-    """Collective Dense Gradient with DP unittests"""
-
-    def setUp(self):
-        """Set up test environment for pytest execution."""
-        # Init the arg parser
-        self.args = layernorm_mlp_grad_parser(["--batch-size", "4"])
-        # Create mesh once for all tests
-        self.args.enable_data_parallel = True
-        self.mesh = _create_mesh(self.args)
-        jax.sharding.set_mesh(self.mesh)
-        self.args.enable_result_check = True
-        os.environ["NVTE_JAX_ALL_REDUCE_IN_FP32"] = "1"
-
-    def tearDown(self):
-        """Clean up after each test."""
-        # Clear the mesh to prevent interference between tests
-        jax.sharding.set_mesh(None)
-        os.environ.pop("NVTE_JAX_ALL_REDUCE_IN_FP32", None)
-
-    def test_te_bf16_layernorm_mlp_grad_with_dp(self):
-        """Test Collective Dense Gradient with AllGather"""
-        run_layernorm_mlp_grad_tests(self.args, self.mesh)
+# class TestCollectiveDenseGradientWithDP(unittest.TestCase):
+#     """Collective Dense Gradient with DP unittests"""
+#
+#     def setUp(self):
+#         """Set up test environment for pytest execution."""
+#         # Init the arg parser
+#         self.args = layernorm_mlp_grad_parser(["--batch-size", "4"])
+#         # Create mesh once for all tests
+#         self.args.enable_data_parallel = True
+#         self.mesh = _create_mesh(self.args)
+#         jax.sharding.set_mesh(self.mesh)
+#         self.args.enable_result_check = True
+#         os.environ["NVTE_JAX_ALL_REDUCE_IN_FP32"] = "1"
+#
+#     def tearDown(self):
+#         """Clean up after each test."""
+#         # Clear the mesh to prevent interference between tests
+#         jax.sharding.set_mesh(None)
+#         os.environ.pop("NVTE_JAX_ALL_REDUCE_IN_FP32", None)
+#
+#     def test_te_bf16_layernorm_mlp_grad_with_dp(self):
+#         """Test Collective Dense Gradient with AllGather"""
+#         run_layernorm_mlp_grad_tests(self.args, self.mesh)
 
 
 if __name__ == "__main__":
