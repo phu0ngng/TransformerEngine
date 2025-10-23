b7491eda02098410247a39987fcfc25c03c06a75
Phuong Nguyen <phuonguyen@nvidia.com>
2025-10-23 09:40:06 -0700

    save
    Signed-off-by: Phuong Nguyen <phuonguyen@nvidia.com>

--- transformer_engine/jax/csrc/extensions/cgemm_helper.cpp
+++ transformer_engine/jax/csrc/extensions/cgemm_helper.cpp
@@ -168,8 +168,9 @@ void CommunicatorHandler::init(int num_total_devices, int num_devices_per_proces
   handler._initialize = true;
 
   // Bootstrap UB via creating a dummy CommOverlapP2PBase object
-  std::vector<size_t> buffer_shape{1, 1};
-  auto _ = CollectiveGemmPlanRegistry::getInstance().get_executor(buffer_shape, DType::kFloat32,
+  std::vector<size_t> buffer_shape{0, 0};
+  // TODO: change dtype here back to FP32
+  auto _ = CollectiveGemmPlanRegistry::getInstance().get_executor(buffer_shape, DType::kBFloat16,
                                                                   JAXX_Collective_Op::ALL_GATHER,
                                                                   true /*is_bootstrap*/);
 }
